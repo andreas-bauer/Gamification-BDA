54a60734c2b6e31411810e75fc119429a2a3b125
Andreas Bauer <dev@andreasbauer.org>
2022-11-19 18:31:27 +0100

    Add setup file to install dependencies

---
+++ setup.R
@@ -0,0 +1,8 @@
+install.packages("devtools") # unless you have it already
+library(devtools)
+install_github("jtextor/dagitty/r")
+devtools::install_github("stan-dev/cmdstanr")
+cmdstanr::install_cmdstan()
+
+install.packages(c("coda", "mvtnorm","loo"))
+devtools::install_github("rmcelreath/rethinking")
